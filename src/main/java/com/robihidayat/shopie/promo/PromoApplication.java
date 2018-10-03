395e9b2d030cedd173f457bbd90239258c88896a
robihidayat <robi.hidayat@mail.ugm.ac.id>
2018-10-03 21:24:29 +0700

    first commit

---
+++ src/main/java/com/robihidayat/shopie/promo/PromoApplication.java
@@ -0,0 +1,12 @@
+package com.robihidayat.shopie.promo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromoApplication.class, args);
+    }
+}
