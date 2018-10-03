0afda3428b9c3c2a0e8cb428f2c29cf9b00ae19f
robihidayat <robi.hidayat@mail.ugm.ac.id>
2018-10-03 23:11:37 +0700

    Add API Documentation README.MD and Swagger.io

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
-# shopie
+# dokumentation shopie
 
 Status build: 
 
 [![CircleCI](https://circleci.com/gh/robihidayat/shopie.svg?style=svg)](https://circleci.com/gh/robihidayat/shopie)
+
+
+### Deployment 
+
+
+1. Check out from github 
+
+2. Installing dependencies
+
+3. Build with docker-compose
+
+4. Running with composer
+
+
+### Documentation Api
+
+
+
+  
---
+++ src/main/java/com/robihidayat/shopie/promo/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.robihidayat.shopie.promo.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author robihidayat
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.robihidayat.shopie.promo.api"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "API Documentation",
+                "This is API Documentation for Shopie API",
+                "0.0.1",
+                "Terms of service",
+                new Contact("Robi Hidayat", "https://shopie.herokuapp.com", "robi.hidayat@mail.ugm.ac.id"),
+                "License of API", "API license URL", new ArrayList<VendorExtension>());
+    }
+
+
+}
